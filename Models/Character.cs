eb1a2d8260a483f3dd493bbbcacfe1e161e8c1bd
Nikita Zaginey <nikitazaginey1@gmail.com>
2022-11-04 21:13:50 +0100

    Add Models

---
+++ Models/Character.cs
@@ -0,0 +1,15 @@
+﻿namespace EditForm.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Bio { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; } = DateTime.Now;
+        public string Image { get; set; } = string.Empty;
+        public int TimeId { get; set; } = 1;
+        public int DifficultyId { get; set; } = 1;
+        public bool IsReadyToFight { get; set; } = true;
+
+    }
+}
